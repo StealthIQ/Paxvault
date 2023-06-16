9574186f2c1ba3e20620bc7697f1a832473c9832
StealthIQ <hiddentesla694@gmail.com>
2023-06-16 20:12:34 +0300

    Added - Password Quality Meter

--- main.py
+++ main.py
@@ -1,9 +1,175 @@
 import os
+import arts
+# from utils.just_banner import *
 import getpass
-import fade
-import time 
+import time
 import webbrowser
-from datetime import datetime 
-# Sub Folder Lib
+from datetime import datetime
+# Sub Folder Lib--------------------------------------
+from utils.password_quality_meter import Main_checker
+from utils.password_generator import password_generator
 from utils.passphrase_generator import passphrase_main
+from utils.sql_database import Passfx_Users_Add, Passfx_login_verify, testing
+from utils.password_manager import pass_manager_main
+# Rich Console Color Lib------------------------------
+from rich.prompt import Prompt
+from rich import print
+# Hashing Libs----------------------------------------
+import hashlib
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+# Get the current date and time
+date_time_now = datetime.now()
+
+# Format the date and time string
+formatted_date_time = date_time_now.strftime("%d-%m-%Y %H:%M:%S")
+date_created = formatted_date_time
+
+# Define a function to encrypt a password using AES-256 encryption
+def encrypt_password(password, key):
+    # Hash the key with SHA-256
+    hashed_key = hashlib.sha256(key.encode()).digest()
+
+    # Initialize the AES cipher with the hashed key
+    cipher = AES.new(hashed_key, AES.MODE_CBC)
+
+    # Pad the password using the new padding method
+    block_size = AES.block_size
+    pad_size = block_size - len(password.encode()) % block_size
+    padding = bytes([pad_size] * pad_size)
+    padded_password = password.encode() + padding
+
+    # Encrypt the padded password
+    encrypted_password = cipher.encrypt(padded_password)
+
+    # Return the encrypted password and the initialization vector
+    return encrypted_password, cipher.iv.hex()
+
+# Define a function to decrypt an encrypted password using AES-256 encryption
+def decrypt_password(encrypted_password_hex, key, iv):
+    # To convert a hex string back to its original bytes form
+    encrypted_password = bytes.fromhex(encrypted_password_hex)
+    # Hash the key using SHA-256
+    hashed_key = hashlib.sha256(key.encode()).digest()
+
+    # Initialize the AES cipher with the hashed key and the initialization vector
+    cipher = AES.new(hashed_key, AES.MODE_CBC, iv=bytes.fromhex(iv))
+
+    # Decrypt the encrypted password
+    decrypted_password = cipher.decrypt(encrypted_password)
+
+    # Unpad the decrypted password using the new unpadding method
+    pad_size = decrypted_password[-1]
+    unpadded_password = decrypted_password[:-pad_size].decode()
+
+    # Return the decrypted password
+    return unpadded_password
+
+
+def Main_modules(username):
+    try:
+        while True:
+            arts.banner()
+            arts.Main_modules()
+            choice = Prompt.ask(f'[yellow]    Enter your choice → [/yellow]')
+            if choice == '1':
+                arts.banner()
+                pass_manager_main(username)
+            elif choice == '2':
+                arts.banner()
+                password_generator()
+                # time.sleep(4)
+            elif choice == '3':
+                arts.banner()
+                passphrase_main()
+            elif choice == '4':
+                arts.banner()
+                Main_checker()
+            elif choice == "Q" or choice == "q":
+                exit()
+            elif choice == "B" or choice == 'b':
+                main()
+            else:
+                print(f'[red]\t ⚠ Invalid choice.')
+    except KeyboardInterrupt:
+        os.system("clear||cls")
+        exit()
+
+
+# Function to login a user
+def login():
+    arts.banner()
+    username = Prompt.ask('\t[x] Enter your username')
+    password = getpass.getpass(prompt='\t[x] Enter the password :', stream=None)
+    username_query_value = Passfx_login_verify(username)
+
+    if username_query_value is not None and username in username_query_value[1]:
+        hashed_pwd = decrypt_password(username_query_value[4], username_query_value[1], username_query_value[5])
+        if password.strip() == hashed_pwd.strip():
+            print(f'\t[green]✓  Login successful!')
+            time.sleep(2)
+            Main_modules(username)
+        else:
+            arts.banner()
+            print(f'\t[red] ⚠  Invalid password.')
+            time.sleep(2)
+    else:
+        arts.banner()
+        print(f'\t[red] ⚠  Invalid username or password.')
+        time.sleep(2)
+
+
+# Function to register a new user
+def register():
+    arts.banner()
+    username = Prompt.ask('\t[x] Enter a username')
+    email = Prompt.ask('\t[x] Enter your email adress')
+    main_key = username
+    password = getpass.getpass(prompt='\t[x] Enter the password :', stream=None).strip()
+    re_password = getpass.getpass(prompt='\t[x] Renter the password :', stream=None).strip()
+    
+    encrypted_password, cipher_iv_password = encrypt_password(password, main_key)
+    encrypted_email, cipher_iv_email = encrypt_password(email, main_key)
+
+
+
+    if password == re_password:
+        Passfx_Users_Add(username, main_key, encrypted_email.hex(), cipher_iv_email, encrypted_password.hex(), cipher_iv_password, date_created)
+        print(f'\n\t[green]✓  User successfully registered!')
+        time.sleep(2)
+    else:
+        arts.banner()
+        print(f"\t[red] ⚠  Passsword Incorrect, try again...")
+        time.sleep(2)
+        register()
+
+
+# Main function to prompt user to register or login
+def main():
+    try:
+        while True:
+            arts.banner()
+            arts.Start_modules()
+            choice = Prompt.ask(f'[yellow]    Enter your choice → ')
+            if choice == '1':
+                login()
+            elif choice == '2':
+                register()
+            elif choice == '3':
+                arts.banner()
+                testing()
+            elif choice == '4':
+                webbrowser.get('firefox').open('https://github.com/StealthIQ')
+            elif choice == "q":
+                exit()
+            else:
+                print(f'[red]\t ⚠ Invalid choice.')
+    except KeyboardInterrupt:
+        os.system("clear||cls")
+        exit()
+
+if __name__ == '__main__':
+    main()
+
 
---
+++ utils/password_quality_meter.py
@@ -0,0 +1,98 @@
+from rich.console import Console
+from rich.progress import Progress
+from rich.table import Table
+from rich import print
+import time
+import os
+import getpass
+import sys
+import pwnedpasswords
+from password_strength import PasswordStats
+
+# Function to check the password strength - x5 Rules
+def check_password_strength(password):
+    # Starting Score
+    score = 0
+    length = len(password)
+    progress = Progress()
+    # Table Tittle etc...
+    rule_table = Table(show_header=True, header_style="bold magenta")
+    rule_table.add_column("Rule")
+    rule_table.add_column("Points", justify="right")
+
+    # Rule 1: Min 8 characters
+    rule1_points = 15 if length >= 8 else 0
+    rule_table.add_row("At least 8 characters", f"[green]{rule1_points}[/green]")
+    score += rule1_points
+
+    # Rule 2: Contains both upper and lowercase characters
+    has_upper = any(c.isupper() for c in password) # ittrates over each character in password c and returns true if any()
+    has_lower = any(c.islower() for c in password) # " "
+    rule2_points = 15 if has_upper and has_lower else 0
+    rule_table.add_row("Contains both uppercase and lowercase characters", f"[green]{rule2_points}[/green]")
+    score += rule2_points
+
+    # Rule 3: Contains numbers
+    has_number = any(c.isdigit() for c in password)
+    rule3_points = 15 if has_number else 0
+    rule_table.add_row("Contains numbers", f"[green]{rule3_points}[/green]")
+    score += rule3_points
+
+    # Rule 4: Contains symbols
+    has_symbol = any(c.isalnum() == False for c in password)
+    rule4_points = 15 if has_symbol else 0
+    rule_table.add_row("Contains symbols", f"[green]{rule4_points}[/green]")
+    score += rule4_points
+
+    # Rule 5: Check if its a common password using pwnedpasswords lib
+    common_passwords = pwnedpasswords.check(password, plain_text=True)
+    rule5_points = 0 if bool(common_passwords) == True else 15
+    rule_table.add_row("Not a common password", f"[green]{rule5_points}[/green]")
+    score += rule5_points
+
+    # Rule 6: Advanced Strength Score
+    stats = PasswordStats(password)
+    extensive_test_points = int((stats.strength()) * 100)/100*25
+    rule_table.add_row("Advanced Strength Test", f"[green]{int(extensive_test_points)}[/green]")
+    score += int(extensive_test_points)
+
+    # Rule table
+    console = Console()
+    console.print("[bold magenta]\n 🔒Password Strength Checker \n[/bold magenta]")
+    console.print(rule_table)
+
+    # Progress bar
+    task = progress.add_task("Scoring", total=100)
+    for i in range(-1, int(score)):
+        progress.advance(task, i)
+        if i <= score:
+            progress.start()
+            time.sleep(0.01)
+            progress.update(task, completed=i+1)
+            # progress.stop()
+            progress.refresh()
+    progress.stop()
+
+    return score
+
+# Main function shit
+def Main_checker():
+    try:
+        password = getpass.getpass(prompt="\n Enter your password → ", stream=None)
+
+        score = check_password_strength(password)
+        print(f"\n[green] :beginner: Your password scored {score}/100 in [bold green]Basic Strength Test")
+        stats = PasswordStats(password)
+        extensive_test = (stats.strength()) * 100
+        print(f"\n[red] :rocket: Your password scored {int(extensive_test)}/100 in [bold red]Advanced Strength Test\n")
+        # xdf = print("\t[bold red]Enter any key to go back →[/bold red]") 
+        input(("Enter any key to go back ..."))
+            # exit()
+
+    except KeyboardInterrupt:
+        os.system("clear||cls")
+        exit()
+
+if __name__ == "__main__":
+    Main_checker()
+
